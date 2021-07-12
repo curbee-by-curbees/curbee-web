55b70d279b1c9ddb5ab9897d9e29da23191380f1
Clem Hepburn <clemhepburn@gmail.com>
2021-07-12 15:09:57 -0700

    pink dot

--- src/app/Footer.js
+++ src/app/Footer.js
@@ -6,7 +6,12 @@ class Footer extends Component {
   render() { 
     return (
       <footer className="Footer">
-        
+        <div className="footer-logo">Curbee<span className='red'>.</span></div>
+        <div className="footer-description">Community Free Stuff Alert System<br />
+          hello@curbee.com<br />
+          <a className="footer-link">Instagram</a>
+          <a className="footer-link">Twitter</a>
+        </div>
       </footer>
     );
   }
--- src/app/Header.js
+++ src/app/Header.js
@@ -7,7 +7,7 @@ class Header extends Component {
     return (
       <header className="Header">
 
-        <h1>Curbee</h1>
+        <h1>Curbee<span className="red">.</span></h1>
         
       </header>
     );
