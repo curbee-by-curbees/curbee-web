d2ecc531aa69e7b18856af00823baf6d1edb133b
Annaleigh Thomas <annaleighxvx@gmail.com>
2021-07-13 17:21:11 -0400

    listingdetail page

--- src/listingDetail/ListingDetail.js
+++ src/listingDetail/ListingDetail.js
@@ -1,12 +1,38 @@
+import { find } from 'async';
 import { Component } from 'react';
+import { getFind } from '../utils/curbee-api';
 import './ListingDetail.css';
+
 
 export default class ListingDetail extends Component {
 
+  state = {
+    find: null
+  }
+
+  async componentDidCatch() {
+    const { match } = this.props;
+    try {
+      const find = await getFind(match.params.id);
+      this.setState({ find: find });
+    }
+    catch (err) {
+      console.log(err.message);
+    }
+  }
+
+  
   render() {
+    const { find } = this.state;
+
     return (
       <div className="ListingDetail">
-        
+
+        <h2>{find.title}</h2>
+        <img src={find.photos[0]} alt={find.title}/>
+        <span>{find.city}</span>
+        <span>{find.category}</span>
+        <span>{find.tags}</span>
       </div>
     );
   }
