602a8d9de8cb1399f78f16293221d908d4b2005c
Clem Hepburn <62565562+clemhepburn@users.noreply.github.com>
2021-07-13 09:33:37 -0700

    Clem css (#4)
    * make some stuff bigger
    
    * fixes to size changes
    
    * Auth route (#3)
    
    * fixed auth page rendering
    
    * implement fonts, alignment, designs
    
    * pink dot
    
    * work toward plugging it in
    
    * fix error
    
    * logging and saving a user
    
    * white spaces
    
    Co-authored-by: Clem Hepburn <clemhepburn@gmail.com>
    
    Co-authored-by: Annaleigh Thomas <annaleighxvx@gmail.com>

--- src/app/Footer.js
+++ src/app/Footer.js
@@ -6,9 +6,9 @@ class Footer extends Component {
   render() { 
     return (
       <footer className="Footer">
-        <div className="footer-logo">Curbee, Inc.<br />1 Free Pile Ave<br />Curb, USA 10001</div>
+        <div className="footer-logo">Curbee<br />1 Free Stuff Ave<br />Curb, USA 10001</div>
         <div className="footer-description">Community Free Stuff Alert System<br />
-          hello@curbee.app<br />
+          hello@curbee.com<br />
           <a className="footer-link">Instagram</a>
           <a className="footer-link">Twitter</a>
         </div>
