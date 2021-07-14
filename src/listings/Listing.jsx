5fa2e6760d5083b2b964e1146ba5fb479698827a
Annaleigh Thomas <annaleighxvx@gmail.com>
2021-07-14 19:26:42 -0400

    cool

--- src/listings/Listing.jsx
+++ src/listings/Listing.jsx
@@ -9,8 +9,8 @@ export default class Listing extends Component {
 
     return (
       <li className="Listing wrapper-h">
-        <img src={(find.photos && find.photos[0]) || 'https:placekitten.com/300/300'} alt={find.title}/>
-        <Link className='find-link' to={`/listings/${find.id}`}>{find.title}</Link>
+        <img src={(find.photos && find.photos[0]) || 'https:placekitten.com/300/300'} alt={find.title} />
+        <Link className='find-link' to={`/listings/${find.id}`}> {find.title}</Link>
         <span className='city'>({find.city})</span>
       </li>
     );
