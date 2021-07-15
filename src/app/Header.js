00a57d4fe8727fa129c23a29ec25d0e1e4eb5d92
Annaleigh Thomas <annaleighxvx@gmail.com>
2021-07-15 15:27:22 -0400

    link to settings and findings

--- src/app/Header.js
+++ src/app/Header.js
@@ -1,13 +1,19 @@
 import { Component } from 'react';
+import { Link } from 'react-router-dom';
 import './Header.css';
 
 class Header extends Component {
+
 
   render() { 
     return (
       <header className="Header">
 
         <h1>Curbee<span className="red">.</span></h1>
+        <Link to="/settings"><img src="setting-gear.png" alt="settings"></img></Link>
+        <Link to="/listings">listings</Link>
+
+    
         
       </header>
     );
