2d85e1cafe6fc1ec518f5a6caf0a1c8b77511c8a
Annaleigh Thomas <annaleighxvx@gmail.com>
2021-07-15 17:16:34 -0400

    removed link

--- src/app/Header.js
+++ src/app/Header.js
@@ -1,5 +1,4 @@
 import { Component } from 'react';
-import { Link } from 'react-router-dom';
 import './Header.css';
 import DropdownButton from 'react-bootstrap/DropdownButton';
 import Dropdown from 'react-bootstrap/Dropdown';
