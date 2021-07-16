d45dd16a23293a6dae6966bb00f2207fab694c6f
culi <culitif@tuta.io>
2021-07-16 09:11:09 -0700

    listings show distance

--- src/listings/Listing.jsx
+++ src/listings/Listing.jsx
@@ -13,7 +13,7 @@ export default class Listing extends Component {
       <li className="Listing wrapper-h">
         <img src={(find.photos && find.photos[0] && find.photos[0].photo) || 'http://placekitten.com/200/300'} alt={find.title} />
         <Link className='find-link' to={`/listings/${find.id}`}> {find.title}</Link>
-        <span className='city'>({find.city})</span>
+        <span className='city'>({(find.distance * 1609.34).toFixed(1)} meters away)</span>
       </li>
     );
   }
